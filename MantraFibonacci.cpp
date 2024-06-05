4e6f3564ad26edb0276d8dab11821a705968a56d
RenggoPandora <161902414+RenggoPandora@users.noreply.github.com>
2024-06-05 14:53:15 +0700

    Add files via upload

---
+++ MantraFibonacci.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+typedef struct stack {
+    int count;
+    int data [];
+};
+stack a;
+void push (stack *x) {
+    x-> data [x->count] = 
+}
+
+void cetak (stack *x) {
+
+}
